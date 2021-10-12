1caa1b052dc318d89064e8cb9bb9132b44936910
bala74923 <bala74923@gmail.com>
2021-10-12 22:40:34 +0530

    Skill rack daily Test 12.10.2021

--- Referral Solutions/Find_Positions_of_Substrings.java
+++ Referral Solutions/Find_Positions_of_Substrings.java
@@ -1,30 +1,28 @@
 // Find Positions of Substrings
 
 import java.util.*;
-public class Hello {
+public class Main {
 
     public static void main(String[] args) {
 		//Your Code Here
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		String first_sub = sc.next();
-		int sub_length = first_sub.length();
-		int total_subs = s.length()/sub_length;
-	    Map<String,Integer> map = getmap(s,sub_length,total_subs);
-	    System.out.println(map.get(first_sub));
+		String first_subString = sc.next();
+		int substring_length = first_subString.length();
+		int total_no_of subs = s.length()/substring_length;
+	    Map<String,Integer> result = getresult(s,substring_length,total_no_of subs);
+	    System.out.println(result.get(first_subString));
 	    while(sc.hasNext())
 	    {
-	        System.out.println(map.get(sc.next()));
+	        System.out.println(result.get(sc.next()));
 	    }
 	}
-	static Map<String,Integer> getmap(String s,int sub_len,int total)
+	static Map<String,Integer> getresult(String s,int sub_len,int total)
 	{
 	    int ind=0;
-	    Map<String,Integer> map = new TreeMap<>();
-	    for(int i=0;i<s.length();i+=sub_len)
-	    {
-	       map.put(s.substring(i,i+sub_len),++ind);
-	    }
-	    return map;
+	    Map<String,Integer> result = new TreeMap<>();
+	    for(int i=0;i<s.length();i+=sub_len) 
+            result.put(s.substring(i,i+sub_len),++ind);
+	    return result;
 	}
 }
