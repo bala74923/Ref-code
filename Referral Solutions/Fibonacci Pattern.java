1c1472ca748d70b76f31840fd456563b3d41f6d2
bala74923 <81730721+bala74923@users.noreply.github.com>
2021-10-13 22:56:32 +0530

    Update Fibonacci Pattern.java

--- Referral Solutions/Fibonacci Pattern.java
+++ Referral Solutions/Fibonacci Pattern.java
@@ -24,7 +24,7 @@ public class Hello {
 		int n=sc.nextInt();
 		String[] arr=new String[n];
 		arr[0]="0"; arr[1]="1";
-		for(int i=2;i<n;++i) if(i<n) arr[i]=arr[i-2]+arr[i-1];
-	    for(int i=0;i<2*n-1;i++) System.out.println(i<n?arr[i]:arr[n-(i%n)-2]);
+		for(int i=2;i<n;++i) arr[i]=arr[i-2]+arr[i-1];
+	        for(int i=0;i<2*n-1;i++) System.out.println(i<n?arr[i]:arr[n-(i%n)-2]);
 	}
 }
